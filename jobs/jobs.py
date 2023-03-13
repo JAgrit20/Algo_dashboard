57040b57340a4ff3b071326fe0c421f3509ac141
Jagrit Acharya <jagritacharya2020@gmail.com>
2023-03-13 09:39:41 +0530

    new count func added

--- jobs/jobs.py
+++ jobs/jobs.py
@@ -73,6 +73,76 @@ def create_row_14min():
     row_data_entry = Vwap_Telegram_data(time=dtobj_indiaa)
     row_data_entry.save()
 
+def show_count():
+    print("Running VWAP")
+
+    print("Telegram_data add")
+    dtobj1 = datetime.datetime.utcnow()  # utcnow class method
+    stockcode = ['ADANIENT', 'ADANIPORTS', 'APOLLOHOSP', 'ASIANPAINT', 'AXISBANK', 'BAJAJ-AUTO', 'BAJFINANCE', 'BAJAJFINSV', 'BPCL', 'BHARTIARTL', 'BRITANNIA', 'CIPLA', 'COALINDIA', 'DIVISLAB', 'DRREDDY', 'EICHERMOT', 'GRASIM', 'HCLTECH', 'HDFCBANK', 'HDFCLIFE', 'HEROMOTOCO', 'HINDALCO',
+                 'HINDUNILVR', 'HDFC', 'ICICIBANK', 'ITC', 'INDUSINDBK', 'INFY', 'JSWSTEEL', 'KOTAKBANK', 'LT', 'M%26M', 'MARUTI', 'NTPC', 'NESTLEIND', 'ONGC', 'POWERGRID', 'RELIANCE', 'SBILIFE', 'SBIN', 'SUNPHARMA', 'TCS', 'TATACONSUM', 'TATAMOTORS', 'TATASTEEL', 'TECHM', 'TITAN', 'UPL', 'ULTRACEMCO', 'WIPRO']
+    # print(stockcode)
+    url = 'https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY'
+
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9'
+    }
+    response = requests.get(url, headers=headers).content
+    data = json.loads(response)
+    count = 0
+    nifty_val = 0
+    nifty_val = data['filtered']['data'][0]['PE']['underlyingValue']
+    print("nifty_val", nifty_val)
+
+    for i in range(len(stockcode)):
+            try:
+                stock_url = 'https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?symbol=' + \
+                    str(stockcode[i])
+                print(stock_url)
+                headers = {
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
+                response = requests.get(stock_url, headers=headers)
+                # response
+                soup = BeautifulSoup(response.text, 'html.parser')
+                data_array = soup.find(id='responseDiv').getText()
+
+                y = json.loads(data_array)
+
+                latest_price = (y['data'][-1]['lastPrice'])
+                latest_price = latest_price.replace(',', '')
+                print("latest", latest_price)
+                latest_price = float(latest_price)
+
+                # name = "SUNPHARMA"
+
+                url = f'https://www1.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?symbol={stockcode[i]}&smeFlag=0&itpFlag=0'
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0'}
+
+                soup = BeautifulSoup(requests.get(
+                    url, headers=headers).content, 'html.parser')
+                data = json.loads(soup.select_one('#responseDiv').text)
+
+                # uncomment this to print all data:
+                # print(json.dumps(data, indent=4))
+                vwap = (data['data'][0]['averagePrice'])
+                vwap = vwap.replace(',', '')
+                vwap = float(vwap)
+                # print("v",type(vwap))
+                # print("latest_price",type(latest_price))
+                vwap = float(vwap)
+
+                print('vwap:', data['data'][0]['averagePrice'])
+
+                if(latest_price > vwap):
+                    count = count + 1
+                # print("yes big")
+                # else:
+                #   # print("small")
+            except Exception as e:
+                print("ERROR : "+str(e))
+    print("current count", count)
 def strategy_5():
 
     print("Running VWAP")
--- jobs/updater.py
+++ jobs/updater.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
-from .jobs import schedule_api,getting_btc_data,Telegram_data,update_token,Place_positional_order,create_row_14min,strategy_5
+from .jobs import schedule_api,getting_btc_data,Telegram_data,show_count,update_token,Place_positional_order,create_row_14min,strategy_5
 from pytz import utc
 from apscheduler.triggers.cron import CronTrigger
 
@@ -8,6 +8,7 @@ def start():
 	scheduler = BackgroundScheduler()
 	scheduler.configure(timezone=utc)
 	scheduler.add_job(create_row_14min, 'interval',  minutes=14)
+	scheduler.add_job(show_count, 'interval',  minutes=5)
 	# scheduler.add_job(schedule_api, 'interval', minutes=15)
 	# scheduler.add_job(clean_daily_db, 'interval', minutes=1)
 	
